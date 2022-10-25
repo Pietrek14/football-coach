29de4cc25337d8b46032d8bc1e850ea76815e550
Pietrek14 <dawidekpe@gmail.com>
2022-10-25 23:42:11 +0200

    added tailwind and made a login form

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,11 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { LoginFormComponent } from './login-form/login-form.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  { path: '', redirectTo: '/home', pathMatch: 'full' },
+  { path: 'login', component: LoginFormComponent }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
