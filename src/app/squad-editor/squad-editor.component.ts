85d7d532d82b098eb16efffdad30666416c0523c
Pietrek14 <dawidekpe@gmail.com>
2022-12-11 17:32:18 +0100

    connected squad editor

--- src/app/squad-editor/squad-editor.component.ts
+++ src/app/squad-editor/squad-editor.component.ts
@@ -1,4 +1,7 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
+import { Squad } from '../squad';
 
 @Component({
   selector: 'app-squad-editor',
@@ -7,11 +10,28 @@ import { Component, OnInit } from '@angular/core';
 })
 export class SquadEditorComponent implements OnInit {
 
+	baseUrl = 'https://football-coach-ad0f1-default-rtdb.europe-west1.firebasedatabase.app/squad/';
+	get squadUrl() {
+		return `${this.baseUrl}${this.id}.json`;
+	}
+	id: number = 0;
+	squad: Squad = new Squad();
 	formActive = false;
 
-  constructor() { }
+  constructor(private http: HttpClient, private route: ActivatedRoute) { }
 
   ngOnInit(): void {
+		this.route.params.subscribe(params => {
+			const id = params['squadId'];
+			if (id) {
+				this.id = id;
+				this.http.get<Squad>(this.squadUrl).subscribe((data: Squad) => {
+					this.squad = data;
+				});
+			} else {
+				console.error('No squad id provided');
+			}
+		});
   }
 
 	openForm() {
@@ -19,7 +39,6 @@ export class SquadEditorComponent implements OnInit {
 	}
 
 	closeForm() {
-		console.log('a');
 		this.formActive = false;
 	}
 
