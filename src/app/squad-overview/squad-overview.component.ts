6e0429fc372d32e9f06d38dfebf88d0987e70bad
Pietrek14 <dawidekpe@gmail.com>
2022-12-12 09:17:44 +0100

    deleting squads

--- src/app/squad-overview/squad-overview.component.ts
+++ src/app/squad-overview/squad-overview.component.ts
@@ -1,26 +1,30 @@
+import { HttpClient } from '@angular/common/http';
 import { Component, Input, OnInit } from '@angular/core';
 
 @Component({
   selector: 'app-squad-overview',
   templateUrl: './squad-overview.component.html',
-  styleUrls: ['./squad-overview.component.css']
+  styleUrls: ['./squad-overview.component.css'],
 })
 export class SquadOverviewComponent implements OnInit {
+  exists = true;
+  squadsUrl =
+    'https://football-coach-ad0f1-default-rtdb.europe-west1.firebasedatabase.app/squad';
 
-	@Input() title = '';
-	@Input() id = 0;
+  @Input() title = '';
+  @Input() id = 0;
 
-  constructor() { }
+  constructor(private http: HttpClient) {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  deleteSquad() {
+    this.http.delete(`${this.squadsUrl}/${this.id}.json`).subscribe((data) => {
+      this.exists = false;
+    });
   }
 
-	deleteSquad() {
-		alert('delete squad ' + this.id);
-	}
-
-	squadLink() {
-		return `/squad/${this.id}`;
-	}
-
+  squadLink() {
+    return `/squad/${this.id}`;
+  }
 }
