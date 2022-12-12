2819c6c5a8f97cbaa2385a6187a9ed2587378858
Pietrek14 <dawidekpe@gmail.com>
2022-12-12 09:12:35 +0100

    bugfix

--- src/app/squads-page/squads-page.component.ts
+++ src/app/squads-page/squads-page.component.ts
@@ -6,31 +6,32 @@ import { Squad } from '../squad';
 @Component({
   selector: 'app-squads-page',
   templateUrl: './squads-page.component.html',
-  styleUrls: ['./squads-page.component.css']
+  styleUrls: ['./squads-page.component.css'],
 })
 export class SquadsPageComponent implements OnInit {
+  squadList: Squad[] = [];
+  squadsUrl =
+    'https://football-coach-ad0f1-default-rtdb.europe-west1.firebasedatabase.app/squad';
 
-	squadList: Squad[] = [];
-	squadsUrl = "https://football-coach-ad0f1-default-rtdb.europe-west1.firebasedatabase.app/squad";
-
-  constructor(private http: HttpClient, private router: Router) { }
+  constructor(private http: HttpClient, private router: Router) {}
 
   ngOnInit(): void {
-		this.fetchSquads();
+    this.fetchSquads();
   }
 
-	fetchSquads() {
-		this.http.get<Squad[]>(this.squadsUrl + ".json").subscribe((data) => {
-			this.squadList = data;
-		});
-	}
+  fetchSquads() {
+    this.http.get<Squad[]>(this.squadsUrl + '.json').subscribe((data) => {
+      this.squadList = data;
+    });
+  }
 
-	addSquad() {
-		const squad = new Squad();
+  addSquad() {
+    const squad = new Squad();
 
-		this.http.put(`${this.squadsUrl}/${this.squadList.length}.json`, squad).subscribe((data) => {
-			this.router.navigate(["/squad", this.squadList.length - 1]);
-		});
-	}
-
+    this.http
+      .put(`${this.squadsUrl}/${this.squadList.length}.json`, squad)
+      .subscribe((data) => {
+        this.router.navigate(['/squad', this.squadList.length]);
+      });
+  }
 }
